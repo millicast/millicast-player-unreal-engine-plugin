0d6b501d28f4179363b2e6f30ec1320a67ddb846
David Baldassin <david.baldassin@cosmosoftware.io>
2022-08-29 14:30:04 +0200

    fix #18: unregister millicast style if already registered

--- Source/MillicastPlayerEditor/Private/MillicastPlayerEditorModule.cpp
+++ Source/MillicastPlayerEditor/Private/MillicastPlayerEditorModule.cpp
@@ -72,6 +72,10 @@ private:
 		StyleInstance->Set("ClassIcon.MillicastTexture2DPlayer",
 			new IMAGE_BRUSH("MillicastMediaSource_20x", Icon20x20));
 
+		// Unregister first if it already exists
+		FSlateStyleRegistry::UnRegisterSlateStyle(*StyleInstance.Get());
+
+		// Register style
 		FSlateStyleRegistry::RegisterSlateStyle(*StyleInstance.Get());
 
 		PlacementModeModule.RegisterPlacementCategory(
